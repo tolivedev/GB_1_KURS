62c8af13b1288c97d261d23dd4ec5e4e18238af9
tolivedev <tolive@live.ru>
2021-08-09 22:42:24 +0300

    lesson 05 done

---
+++ Lessons/05_Lesson/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lessons._05_Lesson
+{
+    class Employee
+    {
+        private Employee()
+        {
+
+        }
+
+        public Employee(string fio, string position, string email, int mobileNum, int salary, int age)
+        {
+            this.FIO = fio;
+            this.Position = position;
+            this.Email = email;
+            this.MobileNum = mobileNum;
+            this.Salary = salary;
+            this.Age = age;
+        }
+        public string FIO { get; private set; }
+        public string Position { get; private set; }
+        public string Email { get; private set; }
+        public int MobileNum { get; private set; }
+        public int Salary { get; private set; }
+        public int Age { get; private set; }
+
+        // Могу обернуть ToString в указанный по заданию метод.
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2} {3} {4} {5}", FIO, Position, Email, MobileNum, Salary, Age);
+        }
+
+    }
+}
---
+++ Lessons/05_Lesson/task01.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace Lessons._05_Lesson
+{
+    class task01
+    {
+        public task01()
+        {
+            Console.WriteLine("Введите произвольный текст, будет записан в файл writeFile.txt\n");
+            ReadAndWriteData();
+        }
+
+        private void ReadAndWriteData()
+        {
+            //System.IO.StreamWriter SW = 
+            using (StreamWriter swr = new StreamWriter(new FileStream("writeFile.txt", FileMode.OpenOrCreate, FileAccess.Write)))
+            {
+                swr.Write(Console.ReadLine());
+                Console.WriteLine("\nWrite done");
+            }
+            Thread.Sleep(2000);
+        }
+    }
+}
